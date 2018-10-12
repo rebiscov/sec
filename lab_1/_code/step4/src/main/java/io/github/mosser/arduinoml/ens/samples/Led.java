8496d8c170314b244dfd0cf98668b66616191626
Guillaume <guillaume.cluzel@ens-lyon.fr>
2018-10-12 17:27:37 +0200

    modification du code de la led pour enlever toutes les conneries que j'ai faites

--- lab_1/_code/step4/src/main/java/io/github/mosser/arduinoml/ens/samples/Led.java
+++ lab_1/_code/step4/src/main/java/io/github/mosser/arduinoml/ens/samples/Led.java
@@ -18,12 +18,12 @@ public class Led {
 		led.setPin(13);
 
 		// Declaring 7-seg
-		Actuator sevenSeg[] = new Actuator[7];
+		/*Actuator sevenSeg[] = new Actuator[7];
 		for(int i = 0; i <= 6; i++) {
 			sevenSeg[i] = new Actuator();
 			sevenSeg[i].setName(String.format("Segment%d", i));
 			sevenSeg[i].setPin(i+1);
-		}
+		}*/
 
 		// Declaring button
 		Sensor button = new Sensor();
@@ -43,7 +43,7 @@ public class Led {
 
 
 		// Declaring states of 7-seg
-		State numbers[] = new State[10];
+		/*State numbers[] = new State[10];
 		for(int i = 0; i < 10; i++) {
 			numbers[i] = new State();
 			numbers[i].setName(String.format("number%d", i));
@@ -59,7 +59,7 @@ public class Led {
 			switchSegOff[i].setActuator(sevenSeg[i]);
 			switchSegOff[i].setValue(SIGNAL.HIGH);
 			switchSegOn[i].setValue(SIGNAL.LOW);
-		}
+		}*/
 
 		// Creating actions
 		Action switchTheLightOn = new Action();
@@ -80,12 +80,12 @@ public class Led {
 		sensorTransitionLedOff.setSensor(button);
 
 		// Creating SensorTransition for the 7-seg
-		SensorTransition changeNumber[] = new SensorTransition[10];
+		/*SensorTransition changeNumber[] = new SensorTransition[10];
 		for(int i = 0 ; i < 10; i++) {
 			changeNumber[i] = new SensorTransition();
 			changeNumber[i].setName(String.format("change%d", i));
 			changeNumber[i].setSensor(button);
-		}
+		}*/
 
 		// Binding actions to states
 		on.setActions(Arrays.asList(switchTheLightOn));
@@ -102,7 +102,7 @@ public class Led {
 		sensorTransitionLedOn.setNextIfLow(on);
 
 
-		numbers[0].setActions(Arrays.asList(switchSegOn[0],switchSegOn[1],switchSegOn[2],switchSegOn[3],switchSegOn[4],switchSegOn[5],switchSegOff[6]));
+		/*numbers[0].setActions(Arrays.asList(switchSegOn[0],switchSegOn[1],switchSegOn[2],switchSegOn[3],switchSegOn[4],switchSegOn[5],switchSegOff[6]));
 		numbers[1].setActions(Arrays.asList(switchSegOff[0],switchSegOn[1],switchSegOn[2],switchSegOff[3],switchSegOff[4],switchSegOff[5],switchSegOff[6]));
 		numbers[2].setActions(Arrays.asList(switchSegOn[0],switchSegOn[1],switchSegOff[2],switchSegOn[3],switchSegOn[4],switchSegOn[5],switchSegOff[6]));
 		numbers[3].setActions(Arrays.asList(switchSegOn[0],switchSegOn[1],switchSegOn[2],switchSegOn[3],switchSegOn[4],switchSegOff[5],switchSegOff[6]));
@@ -119,18 +119,18 @@ public class Led {
 			changeNumber[i].setNextIfHigh(numbers[0]);
 			changeNumber[i].setNextIfLow(numbers[(i+1)%10]);
 		}
-		
+		*/
 
 
 
 		// Building the App
 		App theSwitch = new App();
 		theSwitch.setName("Led!");
-		theSwitch.setBricks(Arrays.asList(led, sevenSeg[0],sevenSeg[1],sevenSeg[2],sevenSeg[6],sevenSeg[3],sevenSeg[4],sevenSeg[5]));
-		theSwitch.setStates(Arrays.asList(on, off,numbers[0], numbers[1],numbers[2], numbers[3],numbers[4], numbers[5],numbers[6], numbers[7], numbers[8], numbers[9]));
+		theSwitch.setBricks(Arrays.asList(led ));//, sevenSeg[0],sevenSeg[1],sevenSeg[2],sevenSeg[6],sevenSeg[3],sevenSeg[4],sevenSeg[5]));
+		theSwitch.setStates(Arrays.asList(on, off)); //,numbers[0], numbers[1],numbers[2], numbers[3],numbers[4], numbers[5],numbers[6], numbers[7], numbers[8], numbers[9]));
 		theSwitch.setSensors(Arrays.asList(button));
-		theSwitch.setSensorTransitions(Arrays.asList(sensorTransitionLedOff, sensorTransitionLedOn,changeNumber[1], changeNumber[7],changeNumber[2], changeNumber[8],changeNumber[3], changeNumber[9],changeNumber[4], changeNumber[5], changeNumber[6], changeNumber[0]));
-		theSwitch.setInitial(Arrays.asList(on, numbers[0]));
+		theSwitch.setSensorTransitions(Arrays.asList(sensorTransitionLedOff, sensorTransitionLedOn)); //,changeNumber[1], changeNumber[7],changeNumber[2], changeNumber[8],changeNumber[3], changeNumber[9],changeNumber[4], changeNumber[5], changeNumber[6], changeNumber[0]));
+		theSwitch.setInitial(Arrays.asList(on)); //, numbers[0]));
 
 		// Generating Code
 		Visitor codeGenerator = new ToC();
